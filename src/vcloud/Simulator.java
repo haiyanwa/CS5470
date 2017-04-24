1eb05caffba5af7cd3e63bb8bd8ea825e71ee72a
wevanw <wevanwg@gmail.com>
2017-04-24 15:55:20 -0700

    add balance fit & fix some bugs

--- src/vcloud/Simulator.java
+++ src/vcloud/Simulator.java
@@ -50,8 +50,16 @@ public class Simulator {
 		return requests;
 	}
 	//show haw many physical servers running in a cloud and how many vh in each server
-	public void showCloud(Cloud cloud){
-		
+	public static void showCloud(Cloud cloud){
+		List<Server> servers = cloud.getServers();
+		System.out.println("server size: " + servers.size());
+		for (int i = 0; i < servers.size(); i++) {
+			Server s = servers.get(i);
+			System.out.printf("Server_%d has %d vh, unused cpu %d", i,
+					s.getVhosts() == null ? 0 : s.getVhosts().size(),
+					s.getCpuUnused());
+			System.out.println();
+		}
 	}
 	
 	public static Cloud firstFit(Cloud cloud, List<Request> requests){
@@ -84,11 +92,12 @@ public class Simulator {
 						vh.setType(request.getType());
 						vh.setVhostStatus(vh.getVhostStatus().RUNNING);
 						s.setCpuUnused(s.getCpuUnused() - vcpu.get(request.getType()));
+						if (s.getVhosts() == null) {
+							s.setVhosts(new ArrayList<>());
+						}
+						s.getVhosts().add(vh);
 						//System.out.println("Server id:" + s.getId() + " left cpu " +  s.getCpuUnused());
-						
-					}else{
-						//check next server
-						continue;
+						break;
 					}
 				}
 				if(vh == null){
@@ -104,6 +113,10 @@ public class Simulator {
 					vh.setType(request.getType());
 					vh.setVhostStatus(vh.getVhostStatus().RUNNING);
 					vh.setServer(newS);
+					if (newS.getVhosts() == null) {
+						newS.setVhosts(new ArrayList<>());
+					}
+					newS.getVhosts().add(vh);
 					servers.add(newS);
 					cloud.setServers(null);
 					cloud.setServers(servers);
@@ -155,8 +168,91 @@ public class Simulator {
 		requests = (ArrayList<Request>) readRequest("./src/vcloud/request.list");
 		
 		
-		c = firstFit(c, requests);
+//		c = firstFit(c, requests);
+//		System.out.println(c.getServers().size());
+
+
+		c = balanceFit(c, requests);
 		System.out.println(c.getServers().size());
+
+		showCloud(c);
+	}
+
+	private static Cloud balanceFit(Cloud cloud, ArrayList<Request> requests) {
+		//get list of servers in the cloud
+		ArrayList<Server> servers = null;
+
+		//process every 10 request as a time slot
+		for(int i=0; i <requests.size(); i=i+10){
+			System.out.println("Request group: " + i);
+
+			servers = (ArrayList<Server>) cloud.getServers();
+			System.out.println("Current server size: " + servers.size());
+
+			//loop through every one of the 10
+			for(int j=i; j<i+10; j++){
+				Vhost vh = null;
+				Request request = requests.get(j);
+				//System.out.println("Serverlist size " + servers.size());
+				System.out.println("Request " + j + ": " + request.getType());
+
+				//check each running server to find availability
+				Server minServer = null;
+				int minLeft = Integer.MAX_VALUE;
+				for(Server s : servers){
+
+					System.out.println("Server_" + s.getId() + " cpu: " + s.getCpuUnused());
+					if(vcpu.get(request.getType()) <= s.getCpuUnused()){
+						if (minServer == null) {
+							minServer = s;
+						}
+						if (s.getCpuUnused() - vcpu.get(request.getType()) < minLeft) {
+							minLeft = s.getCpuUnused() - vcpu.get(request.getType());
+							minServer = s;
+						}
+					}
+				}
+				//best fit
+				if (minServer != null) {
+					//create vhost
+					System.out.println(vcpu.get(request.getType()) + "<" + minServer.getCpuUnused());
+					vh = new Vhost();
+					vh.setServer(minServer);
+					vh.setType(request.getType());
+					vh.setVhostStatus(vh.getVhostStatus().RUNNING);
+					minServer.setCpuUnused(minServer.getCpuUnused() - vcpu.get(request.getType()));
+					if (minServer.getVhosts() == null) {
+						minServer.setVhosts(new ArrayList<>());
+					}
+					minServer.getVhosts().add(vh);
+				}
+				if(vh == null){
+					//no server available
+					//create new server, add to cloud
+					Server newS = new Server(servers.size());
+					System.out.println("Create new server" + newS.getId());
+					newS.setCloud(cloud);
+					newS.setServerStatus(newS.getServerStatus().RUNNING);
+
+					//bring up the new host
+					vh = new Vhost();
+					vh.setType(request.getType());
+					vh.setVhostStatus(vh.getVhostStatus().RUNNING);
+					vh.setServer(newS);
+					if (newS.getVhosts() == null) {
+						newS.setVhosts(new ArrayList<>());
+					}
+					newS.getVhosts().add(vh);
+					servers.add(newS);
+					cloud.setServers(null);
+					cloud.setServers(servers);
+				}
+			}
+			System.out.println("End of group: " + i/10 + " running server : " + servers.size());
+		}
+
+
+		return cloud;
 	}
 
 }
